3bd6a389aea45bd5590c510ed570361326c5de2c
Muhend <hamohand@gmail.com>
2026-08-25 18:19:46 +0200

    chore: retirer training/ du git (2.3 Go)
    Le dossier training/ nest pas nécessaire pour exécuter Dawaliw-ⵣ-OCR.
    Il reste sur le disque mais nest plus suivi par git.
    Cela réduit drastiquement la taille du dépôt et accélère le push.

--- training/scripts/count_gt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-TIF=$(find /home/hamoh/tmz_training/tesstrain/data/tmz_latn-ground-truth -name "*.tif" | wc -l)
-GT=$(find /home/hamoh/tmz_training/tesstrain/data/tmz_latn-ground-truth -name "*.gt.txt" | wc -l)
-echo "🖼  $TIF fichiers .tif"
-echo "📝 $GT fichiers .gt.txt"
-echo "Total: $((TIF + GT)) fichiers"
